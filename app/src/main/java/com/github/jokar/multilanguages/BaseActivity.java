7ea5c73408d6b27df37f65008bffff0092ed3195
JokAr <dongqi.gui@taojiji.com>
2019-05-13 16:55:49 +0800

    更新gradle

--- app/src/main/java/com/github/jokar/multilanguages/BaseActivity.java
+++ app/src/main/java/com/github/jokar/multilanguages/BaseActivity.java
@@ -1,11 +1,6 @@
 package com.github.jokar.multilanguages;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.jokar.multilanguages.utils.LocalManageUtil;

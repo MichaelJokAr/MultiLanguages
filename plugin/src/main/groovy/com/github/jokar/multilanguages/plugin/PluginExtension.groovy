def13388f87cedf2473dae4ed7837b92857dec14
guidongqi <guidongqi@qutoutiao.net>
2020-03-20 10:54:09 +0800

    remove extension

--- plugin/src/main/groovy/com/github/jokar/multilanguages/plugin/PluginExtension.groovy
+++ plugin/src/main/groovy/com/github/jokar/multilanguages/plugin/PluginExtension.groovy
@@ -2,14 +2,11 @@ package com.github.jokar.multilanguages
 
 class PluginExtension {
     boolean enable = true
-    List<String> overwriteClass = new ArrayList<>()
-
 
     @Override
     String toString() {
         return "PluginExtension{" +
                 "enable=" + enable +
-                ", overwriteClass=" + overwriteClass.toString() +
                 '}'
     }
 }
